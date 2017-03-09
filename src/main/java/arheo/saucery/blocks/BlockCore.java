10b25a3ed2a7cb63e1f293a8805089535827400c
Nutsoman <nutsoman666@hotmail.com>
2017-03-09 00:22:09 +0000

    Models

--- src/main/java/arheo/saucery/blocks/BlockCore.java
+++ src/main/java/arheo/saucery/blocks/BlockCore.java
@@ -10,14 +10,29 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class BlockCore extends SaucyBlockWithTile<TileCore>{
+
+    protected static final AxisAlignedBB AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.5D, 1.0D);
+
     public BlockCore() {
         super(Material.ROCK, "core");
         this.registerTile(TileCore.class);
+    }
+
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return AABB;
+    }
+
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
     }
 
     @Override
